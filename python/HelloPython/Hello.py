63a29371a4cfc3323c4bc3d03dc3017deddd6d56
Andrey_K <kaa89job@yandex.ru>
2022-09-23 22:07:33 +0500

    Add HelloPython

---
+++ python/HelloPython/Hello.py
@@ -0,0 +1,58 @@
+# Типы данных и переменная
+# int, fload, boolean, str, list, None
+
+# value = None
+# print(type(value))
+
+# a = 123
+# b = 1.23
+# # print(type(a))
+# # print(type(b))
+# value = 1233345
+# # print(type(value))
+
+# s = 'hello world'
+# print(s) # вывод строки
+# print(a, '-',b, '-',s)
+# print('{} - {} - {}'.format(a, b, s))  # формат
+# print(f'{a} - {b} - {s}') # интерполяция
+# print('{1} - {2} - {0}'.format(a, b, s))
+
+# f = False
+# print(f)
+
+# list = ['1', '2', '3', 'hello', 1,2,3,4,5,True]
+# print(list)
+
+# Ввод и вывод данных
+# print, input
+
+# print('Введите a');
+# a = int(input())    # цифровой формат, целое число
+# print('Введите b');
+# b = int(input())
+# print(a, ' + ' , b, ' = ' , a+b)
+# print('{} {}'.format(a, b))
+# print(f'{a} {b}')
+
+# print('Введите a');
+# a = float(input())    # цифровой формат, вещественное число
+# print('Введите b');
+# b = float(input())
+# print(a, ' + ' , b, ' = ' , a+b)
+
+# Арифметические операции
+#  +, -, *, /, %, //,**
+#  **,+,-,*,/,//,%,+,-
+# (), Сокращенные операции
+
+# a = 1.3123
+# b = 3
+# c = round(a * b, 5)
+# print(c)
+
+# a = 3
+# a *= 5
+
+# print(a)
+
