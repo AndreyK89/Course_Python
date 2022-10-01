81ba04cb268ad57c8e00b0c7132e68953399e3ff
Andrey_K <kaa89job@yandex.ru>
2022-10-02 00:23:20 +0500

    Add Task 4

---
+++ Seminars/Homework/DZ 3/Task 4.py
@@ -0,0 +1,16 @@
+# Выполнил Андрей Кошелев Аналитика | 6 | 3040 | 21.09.2022
+# Задача 4 - Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Пример:
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+def conversion_binary(number):
+    binary_number = ''
+    while number !=0:
+        binary_number = str(number%2) + binary_number
+        number //=2
+    return binary_number
+
+number = int(input('Введите число: '))
+print(conversion_binary(number))
