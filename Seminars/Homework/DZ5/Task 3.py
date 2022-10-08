ad015b44a24236e80410e946f4c59759642395c5
Andrey_K <kaa89job@yandex.ru>
2022-10-08 23:42:01 +0500

    Add Task 3

---
+++ Seminars/Homework/DZ5/Task 3.py
@@ -0,0 +1,36 @@
+# Выполнил Андрей Кошелев Аналитика | 6 | 3040 | 21.09.2022
+# Задача 3: Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Пример:
+# На сжатие входные данные: aaabbbccccdddddeeeee
+# Выходные данные:          3a3b4c5d5e
+
+
+
+def rle_coding(data):
+    count = 1
+    res = ''
+    for i in range(len(data)-1):
+        if data[i] == data[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + data[i]
+            count = 1
+    if count > 1 or (data[len(data)-2] != data[-1]):
+        res = res + str(count) + data[-1]
+    return res
+
+def decoding(data):
+    number = ''
+    res = ''
+    for i in range(len(data)):
+        if not data[i].isalpha():
+            number += data[i]
+        else:
+            res = res + data[i] * int(number)
+            number = ''
+    return res
+
+
+s = input("Введите текст для сжатия данных: ")
+print(f"Данные после сжатия: {rle_coding(s)}")
+print(f"Данные после дешифровки: {decoding(rle_coding(s))}")
