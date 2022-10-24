6661ba52718633774c1bb9d9006ce01c9ed80a20
Andrey_K <kaa89job@yandex.ru>
2022-10-25 00:21:55 +0500

    Edit Telebot.py

--- Seminars/Homework/DZ 9/TelebotFT/Telebot.py
+++ Seminars/Homework/DZ 9/TelebotFT/Telebot.py
@@ -1,36 +1,34 @@
 import telebot
 import random
 from telebot import types
-# Загружаем список интересных фактов
-f = open('facts.txt', 'r', encoding='UTF-8')
+
+f = open('facts.txt', 'r', encoding='UTF-8')                       # Загружаем список интересных фактов
 facts = f.read().split('\n')
 f.close()
-# Загружаем список поговорок
-f = open('thinks.txt', 'r', encoding='UTF-8')
+
+f = open('thinks.txt', 'r', encoding='UTF-8')                      # Загружаем список поговорок
 thinks  = f.read().split('\n')
 f.close()
-# Создаем бота
-bot = telebot.TeleBot('5671094069:AAE8MGojJf9ScZm_tP8bJMpp5BbtkbwN47E')
-# Команда start
-@bot.message_handler(commands=["start"])
+
+bot = telebot.TeleBot('5671094069:AAE8MGojJf9ScZm_tP8bJMpp5BbtkbwN47E')    # Создаем бота
+
+@bot.message_handler(commands=["start"])                              # Команда start
 def start(m, res=False):
-        # Добавляем две кнопки
-        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)        # Добавляем две кнопки
         item1=types.KeyboardButton("Факт о Тобольске")
         item2=types.KeyboardButton("Поговорка Тобольской губернии")
         markup.add(item1)
         markup.add(item2)
         bot.send_message(m.chat.id, 'Нажми: \n \nФакт для получения интересного факта о Тобольске\n \nПоговорка Тобольской губернии — для получения мудрой цитаты ',  reply_markup=markup)
-# Получение сообщений от юзера
-@bot.message_handler(content_types=["text"])
+
+@bot.message_handler(content_types=["text"])                         # Получение сообщений от юзера
 def handle_text(message):
-    # Если юзер прислал 1, выдаем ему случайный факт
+
     if message.text.strip() == 'Факт о Тобольске' :
-            answer = random.choice(facts)
-    # Если юзер прислал 2, выдаем умную мысль
+            answer = random.choice(facts)                             # Если юзер прислал 1, выдаем ему случайный факт
     elif message.text.strip() == 'Поговорка Тобольской губернии':
-            answer = random.choice(thinks)
-    # Отсылаем юзеру сообщение в его чат
-    bot.send_message(message.chat.id, answer)
-# Запускаем бота
-bot.polling(none_stop=True, interval=0)
+            answer = random.choice(thinks)                            # Если юзер прислал 2, выдаем умную мысль
+    bot.send_message(message.chat.id, answer)                         # Отсылаем юзеру сообщение в его чат
+
+bot.polling(none_stop=True, interval=0)                               # Запускаем бота
